2a19659afac35fe35bab01340521afb176dd0b40
JuanBeta-exe <juanbetar7@gmail.com>
2025-03-03 00:19:07 -0500

    Subiendo app con timer

---
+++ 911/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace _911
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picbxTorresGenelas = new System.Windows.Forms.PictureBox();
+            this.bttAtentar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxTorresGenelas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picbxTorresGenelas
+            // 
+            this.picbxTorresGenelas.Image = global::_911.Properties.Resources.Torres_Gemelas;
+            this.picbxTorresGenelas.Location = new System.Drawing.Point(418, 12);
+            this.picbxTorresGenelas.Name = "picbxTorresGenelas";
+            this.picbxTorresGenelas.Size = new System.Drawing.Size(141, 197);
+            this.picbxTorresGenelas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picbxTorresGenelas.TabIndex = 0;
+            this.picbxTorresGenelas.TabStop = false;
+            // 
+            // bttAtentar
+            // 
+            this.bttAtentar.Location = new System.Drawing.Point(432, 401);
+            this.bttAtentar.Name = "bttAtentar";
+            this.bttAtentar.Size = new System.Drawing.Size(127, 35);
+            this.bttAtentar.TabIndex = 1;
+            this.bttAtentar.Text = "ATENTAR";
+            this.bttAtentar.UseVisualStyleBackColor = true;
+            this.bttAtentar.Click += new System.EventHandler(this.bttAtentar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 583);
+            this.Controls.Add(this.bttAtentar);
+            this.Controls.Add(this.picbxTorresGenelas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picbxTorresGenelas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picbxTorresGenelas;
+        private System.Windows.Forms.Button bttAtentar;
+    }
+}
+
---
+++ 911/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _911
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox misil;
+        private Timer timerTorre;
+        private Timer timerMisil;
+        private int velocidadAvion = 5;
+        private int velocidadMisil = 10;
+        private bool misilDisparado = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarJuego();
+        }
+
+        private void InicializarJuego()
+        {
+
+            misil = new PictureBox
+            {
+                Image = Properties.Resources.Avion,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Size = new Size(60, 120),
+                Visible = false // Oculto hasta que se dispare
+            };
+            this.Controls.Add(misil);
+
+            // Timer para mover el avión
+            timerTorre = new Timer { Interval = 50 };
+            timerTorre.Tick += MoverAvion;
+            timerTorre.Start();
+
+            // Timer para mover el misil
+            timerMisil = new Timer { Interval = 30 };
+            timerMisil.Tick += MoverMisil;
+
+            // Evento para disparar el misil con el clic del mouse
+            this.MouseClick += Form1_MouseClick;
+        }
+
+        private void MoverAvion(object sender, EventArgs e)
+        {
+            // Mueve el avión de lado a lado
+            picbxTorresGenelas.Left += velocidadAvion;
+
+            // Si el avión toca los bordes, cambia de dirección
+            if (picbxTorresGenelas.Right >= this.ClientSize.Width || picbxTorresGenelas.Left <= 0)
+            {
+                velocidadAvion = -velocidadAvion;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!misilDisparado)
+            {
+                misil.Location = new Point(e.X - misil.Width / 2, this.ClientSize.Height - misil.Height);
+                misil.Visible = true;
+                misilDisparado = true;
+                timerMisil.Start();
+            }
+        }
+
+        private void MoverMisil(object sender, EventArgs e)
+        {
+            if (misilDisparado)
+            {
+                misil.Top -= velocidadMisil;
+
+                // Verificar colisión con el avión
+                if (avion.Bounds.IntersectsWith(picbxTorresGenelas.Bounds))
+                {
+                    timerMisil.Stop();
+                    misil.Visible = false;
+                    misilDisparado = false;
+                    MessageBox.Show("¡Avión derribado!");
+                }
+
+                // si el misil sale de la pantalla se oculta y se reinicia
+                if (misil.Top <= 0)
+                {
+                    timerMisil.Stop();
+                    misil.Visible = false;
+                    misilDisparado = false;
+                }
+            }
+        }
+
+
+        private void bttAtentar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
